64e5917fafb562586633f97c6f375185c087f403
29Jeanca <jeancarlosbarberena29@gmail.com>
2025-09-20 15:52:24 -0600

    commit

--- src/components/InvitationCard.jsx
+++ src/components/InvitationCard.jsx
@@ -1,28 +1,45 @@
+import { Link } from "react-router-dom";
 import "../styles/InvitationCard.css";
-const InvitationCard = ({ guestName, invitationMessage, table }) => {
-  return (
-    <>
-      <div className="invitation-card">
-        <div className="invitation-header">
-          <img
-            src="/imgs/imgInvitationCard.png"
-            alt="Logo"
-            className="invitation-logo"
-          />
 
-          <h1 className="guest-name">{guestName}</h1>
-        </div>
-        <hr />
-        <p className="invitation-message">{invitationMessage}</p>
-        <div>
-          <p className="invitation-details">Fecha: 21 de Septiembre</p>
-          <p className="invitation-details">Hora: 5:00 PM</p>
-          <p className="invitation-details">Lugar: Confirmar</p>
-          <p className="invitation-details">Mesa #{table}</p>
-          <p className="invitation-details">enlace waze</p>
+const InvitationCard = ({ guestName, table }) => {
+  return (
+    <div className="invitation-card">
+      {/* Ositos en esquinas */}
+      <img src="/imgs/imgInvitationCard.png" alt="Osito" className="teddy top-left" />
+      <img src="/imgs/imgInvitationCard.png" alt="Osito" className="teddy top-right" />
+      <img src="/imgs/imgInvitationCard.png" alt="Osito" className="teddy bottom-left" />
+      <img src="/imgs/imgInvitationCard.png" alt="Osito" className="teddy bottom-right" />
+
+      {/* Encabezado con arco, lazo y banner */}
+      <div className="invitation-header">
+        <img
+          src="/imgs/imglazo2.png"
+          alt="Lazo"
+          className="invitation-bow"
+        />
+        <div className="invitation-banner">
+          <h2>Mesa {table}</h2>
         </div>
       </div>
-    </>
+
+      {/* Nombre del invitado */}
+      <div className="invitation-body">
+        <h3 className="guest-name">{guestName}</h3>
+        <p className="invitation-details">Fecha: 21 de Septiembre</p>
+        <p className="invitation-details">Hora: 5:00 PM</p>
+        <p className="invitation-details">
+          Lugar: <span className="italic-text">Hacienda Salitrillos</span>
+        </p>
+        <p className="invitation-details enlace">
+          <Link to="https://www.waze.com/es/live-map/directions/hacienda-salitrillos-salitrillos,-aserri?to=place.w.180813922.1808204761.7520175">
+            Waze
+          </Link>
+        </p>
+        <p>Recuerda llevar una prenda blanca</p>
+        <h3>Te esperamos</h3>
+      </div>
+    </div>
   );
 };
+
 export default InvitationCard;
--- src/components/SeachGuest.jsx
+++ src/components/SeachGuest.jsx
@@ -1,40 +1,53 @@
-import { useEffect } from "react";
 import { useState } from "react";
-import "../styles/SearchGuest.css"
+import "../styles/SearchGuest.css";
 import { useApi } from "../../public/services/useApi";
 import "../../public/services/guests.json";
 import { useNavigate } from "react-router-dom";
-const SearchGuest = () =>{
-    const navigate = useNavigate();
-    const [fullName, setFullName] = useState("");
-    const { data, loading, error, request } = useApi('/services/guests.json');
-    
-    const searchName = async () =>{
-         if (!fullName.trim()) {
-            alert("Por favor ingresa un nombre válido");
-            return;
-         }
-         const users = await request("")
-          const guest = users ? users.guests : [];
-         if(!users) return alert("No se pudo cargar la lista de invitados, intentalo mas tarde")
-            const user = guest.find(user => user.nombre.toLowerCase().trim() === fullName.toLowerCase().trim());
-         if(user){
-            localStorage.setItem("guestName", user.nombre);
-            localStorage.setItem("table", user.mesa);
-            navigate("/inicio");
-            return
-        }
-        return alert("No se encontró el nombre, puede ser que lo hayas escrito mal!")
-   }   
-   
-    return(
-        <>
-            <h2>¡Escribe tu nombre completo, y sabrás donde sentarte!</h2>
-            <input placeholder="Nombre Completo" className="input-searchGuest" onChange={(e)=>setFullName(e.target.value)}/>
-            <button onClick={searchName} className="btn-searchGuest">Buscar</button>
-            {loading && <p>Cargando...</p>}
-            {error && <p>Error: {error}</p>}
-        </>
-    )
-}
-export default SearchGuest
+const SearchGuest = () => {
+  const navigate = useNavigate();
+  const [fullName, setFullName] = useState("");
+  const { data, loading, error, request } = useApi("/services/guests.json");
+
+  const searchName = async () => {
+    if (!fullName.trim()) {
+      alert("Por favor ingresa un nombre válido");
+      return;
+    }
+    const users = await request("");
+    const guest = users ? users.guests : [];
+    if (!users)
+      return alert(
+        "No se pudo cargar la lista de invitados, intentalo mas tarde"
+      );
+    const user = guest.find(
+      (user) =>
+        user.nombre.toLowerCase().trim() === fullName.toLowerCase().trim()
+    );
+    if (user) {
+      localStorage.setItem("guestName", user.nombre);
+      localStorage.setItem("table", user.mesa);
+      navigate("/inicio");
+      return;
+    }
+    return alert(
+      "No se encontró el nombre, puede ser que lo hayas escrito mal!"
+    );
+  };
+
+  return (
+    <div className="search-container">
+      <h2>¡Escribe tu nombre completo, y sabrás donde sentarte!</h2>
+      <input
+        placeholder="Nombre Completo"
+        className="input-searchGuest"
+        onChange={(e) => setFullName(e.target.value)}
+      />
+      <button onClick={searchName} className="btn-searchGuest">
+        Buscar
+      </button>
+      {loading && <p>Cargando...</p>}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
+};
+export default SearchGuest;
